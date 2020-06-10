817dcf604a29d3f44ada5b1bf7f025452c4be501
arnie b <arnold.bwaila@gmail.com>
2020-06-10 19:43:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,13 +60,13 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(num) 
+  def self.first_student_in_grade_10
     sql = <<-SQL
           SELECT * FROM students 
           WHERE students.grade = '10'
-          LIMIT ?
+          LIMIT 1
           SQL
-    DB[:conn].execute(sql,num).collect do |row|
+    DB[:conn].execute(sql).collect do |row|
       Song.new_from_db(row)
     end
   end
