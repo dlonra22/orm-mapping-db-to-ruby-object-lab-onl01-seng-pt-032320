32c33f71caeef262dad6ad048c3ab1285d6f8ed7
arnie b <arnold.bwaila@gmail.com>
2020-06-10 19:50:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
     end.first
   end
   
-  def all_students_in_grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL
           SELECT * FROM students 
           WHERE students.grade = '9'
